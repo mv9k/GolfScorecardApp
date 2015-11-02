7118e13629c1f7f9783851f7cf228dd3538be3bc
Kris <kris000@gmail.com>
2015-11-02 10:57:25 -0700

    bunch of stuff

--- js/GolfApp.js
+++ js/GolfApp.js
@@ -1,7 +1,7 @@
 /**
  * Created by kris on 10/29/15.
  */
-document.getElementById("coursename").innerHTML =  model.course.name;
+
 
 function saveData() {
     localStorage.setItem("player1name", document.getElementById("player1").value);
@@ -108,45 +108,91 @@ function loadData() {
     document.getElementById("player1hole16").value = localStorage.getItem("player1hole16");
     document.getElementById("player1hole17").value = localStorage.getItem("player1hole17");
     document.getElementById("player1hole18").value = localStorage.getItem("player1hole18");
+    document.getElementById("player1totalscore").innerText = localStorage.getItem("player1totalscore");
+
+    document.getElementById("player2").value = localStorage.getItem("player2name");
+    document.getElementById("player2hole1").value = localStorage.getItem("player2hole1");
+    document.getElementById("player2hole2").value = localStorage.getItem("player2hole2");
+    document.getElementById("player2hole3").value = localStorage.getItem("player2hole3");
+    document.getElementById("player2hole4").value = localStorage.getItem("player2hole4");
+    document.getElementById("player2hole5").value = localStorage.getItem("player2hole5");
+    document.getElementById("player2hole6").value = localStorage.getItem("player2hole6");
+    document.getElementById("player2hole7").value = localStorage.getItem("player2hole7");
+    document.getElementById("player2hole8").value = localStorage.getItem("player2hole8");
+    document.getElementById("player2hole9").value = localStorage.getItem("player2hole9");
+    document.getElementById("player2hole10").value = localStorage.getItem("player2hole10");
+    document.getElementById("player2hole11").value = localStorage.getItem("player2hole11");
+    document.getElementById("player2hole12").value = localStorage.getItem("player2hole12");
+    document.getElementById("player2hole13").value = localStorage.getItem("player2hole13");
+    document.getElementById("player2hole14").value = localStorage.getItem("player2hole14");
+    document.getElementById("player2hole15").value = localStorage.getItem("player2hole15");
+    document.getElementById("player2hole16").value = localStorage.getItem("player2hole16");
+    document.getElementById("player2hole17").value = localStorage.getItem("player2hole17");
+    document.getElementById("player2hole18").value = localStorage.getItem("player2hole18");
+    document.getElementById("player2totalscore").innerText = localStorage.getItem("player2totalscore");
+
+    document.getElementById("player3").value = localStorage.getItem("player3name");
+    document.getElementById("player3hole1").value = localStorage.getItem("player3hole1");
+    document.getElementById("player3hole2").value = localStorage.getItem("player3hole2");
+    document.getElementById("player3hole3").value = localStorage.getItem("player3hole3");
+    document.getElementById("player3hole4").value = localStorage.getItem("player3hole4");
+    document.getElementById("player3hole5").value = localStorage.getItem("player3hole5");
+    document.getElementById("player3hole6").value = localStorage.getItem("player3hole6");
+    document.getElementById("player3hole7").value = localStorage.getItem("player3hole7");
+    document.getElementById("player3hole8").value = localStorage.getItem("player3hole8");
+    document.getElementById("player3hole9").value = localStorage.getItem("player3hole9");
+    document.getElementById("player3hole10").value = localStorage.getItem("player3hole10");
+    document.getElementById("player3hole11").value = localStorage.getItem("player3hole11");
+    document.getElementById("player3hole12").value = localStorage.getItem("player3hole12");
+    document.getElementById("player3hole13").value = localStorage.getItem("player3hole13");
+    document.getElementById("player3hole14").value = localStorage.getItem("player3hole14");
+    document.getElementById("player3hole15").value = localStorage.getItem("player3hole15");
+    document.getElementById("player3hole16").value = localStorage.getItem("player3hole16");
+    document.getElementById("player3hole17").value = localStorage.getItem("player3hole17");
+    document.getElementById("player3hole18").value = localStorage.getItem("player3hole18");
+    document.getElementById("player3totalscore").innerText = localStorage.getItem("player3totalscore");
+
+    document.getElementById("player4").value = localStorage.getItem("player4name");
+    document.getElementById("player4hole1").value = localStorage.getItem("player4hole1");
+    document.getElementById("player4hole2").value = localStorage.getItem("player4hole2");
+    document.getElementById("player4hole3").value = localStorage.getItem("player4hole3");
+    document.getElementById("player4hole4").value = localStorage.getItem("player4hole4");
+    document.getElementById("player4hole5").value = localStorage.getItem("player4hole5");
+    document.getElementById("player4hole6").value = localStorage.getItem("player4hole6");
+    document.getElementById("player4hole7").value = localStorage.getItem("player4hole7");
+    document.getElementById("player4hole8").value = localStorage.getItem("player4hole8");
+    document.getElementById("player4hole9").value = localStorage.getItem("player4hole9");
+    document.getElementById("player4hole10").value = localStorage.getItem("player4hole10");
+    document.getElementById("player4hole11").value = localStorage.getItem("player4hole11");
+    document.getElementById("player4hole12").value = localStorage.getItem("player4hole12");
+    document.getElementById("player4hole13").value = localStorage.getItem("player4hole13");
+    document.getElementById("player4hole14").value = localStorage.getItem("player4hole14");
+    document.getElementById("player4hole15").value = localStorage.getItem("player4hole15");
+    document.getElementById("player4hole16").value = localStorage.getItem("player4hole16");
+    document.getElementById("player4hole17").value = localStorage.getItem("player4hole17");
+    document.getElementById("player4hole18").value = localStorage.getItem("player4hole18");
+    document.getElementById("player4totalscore").innerText = localStorage.getItem("player4totalscore");
 }
 
 
-function numOfPlayers(num) {
-    if (num == 1){
-        document.getElementById("player1card").style.display= "block";
-        document.getElementById("player2card").style.display= "none";
-        document.getElementById("player3card").style.display= "none";
-        document.getElementById("player4card").style.display= "none";
-    }
-    if (num == 2){
-        document.getElementById("player1card").style.display= "none";
-        document.getElementById("player2card").style.display= "block";
-        document.getElementById("player3card").style.display= "none";
-        document.getElementById("player4card").style.display= "none";
-    }
-    if (num == 3){
-        document.getElementById("player1card").style.display= "none";
-        document.getElementById("player2card").style.display= "none";
-        document.getElementById("player3card").style.display= "block";
-        document.getElementById("player4card").style.display= "none";
-    }
-    if (num == 4) {
-        document.getElementById("player1card").style.display= "none";
-        document.getElementById("player2card").style.display= "none";
-        document.getElementById("player3card").style.display= "none";
-        document.getElementById("player4card").style.display= "block";
-    }
 
-}
+
 
 function onload() {
 
+
     loadData();
+
+    setPars();
 
     document.getElementById("player1card").style.display= "block";
     document.getElementById("player2card").style.display= "none";
     document.getElementById("player3card").style.display= "none";
     document.getElementById("player4card").style.display= "none";
+
+    document.getElementById("coursename").innerHTML =  model.course.name;
+
+
 
 // Oct 29th
     function newPlayer(name) {
@@ -180,7 +226,33 @@ function onload() {
     }
 }
 
+function numOfPlayers(num) {
+    if (num == 1){
+        document.getElementById("player1card").style.display= "block";
+        document.getElementById("player2card").style.display= "none";
+        document.getElementById("player3card").style.display= "none";
+        document.getElementById("player4card").style.display= "none";
+    }
+    if (num == 2){
+        document.getElementById("player1card").style.display= "none";
+        document.getElementById("player2card").style.display= "block";
+        document.getElementById("player3card").style.display= "none";
+        document.getElementById("player4card").style.display= "none";
+    }
+    if (num == 3){
+        document.getElementById("player1card").style.display= "none";
+        document.getElementById("player2card").style.display= "none";
+        document.getElementById("player3card").style.display= "block";
+        document.getElementById("player4card").style.display= "none";
+    }
+    if (num == 4) {
+        document.getElementById("player1card").style.display= "none";
+        document.getElementById("player2card").style.display= "none";
+        document.getElementById("player3card").style.display= "none";
+        document.getElementById("player4card").style.display= "block";
+    }
 
+}
 
 
 // AJAX request with swingbyswing API
@@ -235,6 +307,10 @@ function initMap(long, hole1location, tee1location) {
 
     });
 
+}
+
+function setPars() {
+    document.getElementsByName("player1hole1")[0].placeholder = "Par " + model.course.name;
 }
 
 
